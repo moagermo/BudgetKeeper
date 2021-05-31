980832a6057184390273e34cc66141b3afe389f1
Mo Ager <moager@att.net>
2021-05-31 15:36:24 -0400

    Deployment

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
     useFindAndModify: false
   }
 );
